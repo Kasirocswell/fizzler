4952adfa7a271533341275fdff543e134f76272c
Gregory Reeves <gregory.reeves@ITSM-650.attlocal.net>
2023-11-08 12:58:15 -0800

    Navbar effects, contact page

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -8,23 +8,37 @@ import styles from './Navbar.module.css';
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
+  const navItems = [
+    { href: '/about', label: 'About', color: 'var(--blue)' }, // Blue
+    { href: '/flavors', label: 'Flavors', color: 'var(--pink)' }, // Pink
+    { href: '/contact', label: 'Contact', color: 'var(--green)' }, // Green
+    // Add more items here
+  ];
+
   return (
     <nav className="bg-white shadow-lg">
-      <div className="max-w-full mx-auto lg:px-2 xl:px-0"> {/* Adjusted for larger screens */}
-        <div className="flex justify-between items-center h-16"> {/* Added h-16 for consistent height */}
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
           {/* Website Logo */}
-          <a href="/" className="flex items-center py-4 lg:py-0 ml-2 md:ml-8"> {/* Adjusted padding for larger screens */}
+          <a href="/" className="flex items-center py-4 px-2">
             <Image src="/tlogo.png" alt="Logo" width={100} height={100} />
           </a>
           {/* Primary Navbar items */}
-          <div className="hidden md:flex items-center space-x-4 mr-10">
-            <a href="/about" className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold ">About</a>
-            <a href="/flavors" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Flavors</a>
-            <a href="#" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">Contact</a>
+          <div className="hidden md:flex items-center space-x-4">
+            {navItems.map((item, index) => (
+              <a 
+                href={item.href} 
+                className={`py-4 px-2 font-semibold ${styles.navLink}`} 
+                style={{ '--c': item.color } as React.CSSProperties} // Type assertion here
+                key={index}
+              >
+                {item.label}
+              </a>
+            ))}
           </div>
           {/* Mobile menu button */}
-          <div className="md:hidden flex items-center mr-5">
-            <button type="button" className="text-gray-500 hover:text-green-500 focus:outline-none focus:text-green-500" aria-label="toggle menu" onClick={() => setIsOpen(!isOpen)}>
+          <div className="md:hidden flex items-center">
+            <button type="button" onClick={() => setIsOpen(!isOpen)}>
               <IconMenu2 size={30} />
             </button>
           </div>
@@ -32,9 +46,16 @@ const Navbar: React.FC = () => {
       </div>
       {/* Mobile Menu */}
       <div className={`${isOpen ? "block" : "hidden"} md:hidden ${styles.mobileMenuEnter}`}>
-        <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">About</a>
-        <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">Flavors</a>
-        <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">Contact</a>
+        {navItems.map((item, index) => (
+          <a 
+            href={item.href} 
+            className={`block py-2 px-4 text-sm ${styles.navLink}`} 
+            style={{ '--c': item.color } as React.CSSProperties} // Type assertion here
+            key={index}
+          >
+            {item.label}
+          </a>
+        ))}
       </div>
     </nav>
   );
